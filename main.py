ff7867175380fbfabc13a6d464b54878651a9b84
Saentinel <stefan.pippke@gmail.com>
2025-10-13 14:09:28 +0200

    20251013: Creating Player Triangle!

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
+
 
 def main():
-    pygame.init()  # initialize the pygame library
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    color = (0,0,0)
-    dt = 0
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+    dt = 0
+
+
     while True:
-        screen.fill(color)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        clock.tick(60)
-        dt = clock.tick() / 1000
+        
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
