8a7ef1c4c9fef0088aa371fbd2ade7155986bf2f
Saentinel <stefan.pippke@gmail.com>
2025-10-13 08:25:23 +0200

    20251013: black Screen created

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+    pygame.init()  # initialize the pygame library
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    color = (0,0,0)
+    while True:
+        screen.fill(color)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
