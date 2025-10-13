82b9873de90bacab0abcb26c74d723cdab1f6505
Saentinel <stefan.pippke@gmail.com>
2025-10-13 08:40:01 +0200

    20251013: create FPS Limit!

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
     pygame.init()  # initialize the pygame library
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0,0,0)
+    dt = 0
+    clock = pygame.time.Clock()
     while True:
         screen.fill(color)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
+        dt = clock.tick() / 1000
 
 
 if __name__ == "__main__":
