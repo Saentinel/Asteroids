6c5e3af74d15fa75ca14d1abfc7ac8cd10260e05
Saentinel <stefan.pippke@gmail.com>
2025-10-14 08:43:47 +0200

    20251014: Creating AsteroidField!

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroid = pygame.sprite.Group()
+    
 
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroid, updatable, drawable)   
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
