2d809c6f0c374abfa4af5ed559f44ab2ddeff300
Saentinel <stefan.pippke@gmail.com>
2025-11-07 20:42:51 +0100

    20251107: Logger added!

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from shot import Shot
 
 
 def main():
+    
     pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
